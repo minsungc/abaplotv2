7342b7f1fe6acf50650dc0c9c14e28b077bb1c70
Minsung Cho <minsung@ccs.neu.edu>
2023-04-23 23:35:40 -0400

    barycentrics

--- Abaplot/monads/barycentric.lean
+++ Abaplot/monads/barycentric.lean
@@ -8,7 +8,7 @@ import Mathlib.Algebra.BigOperators.Basic
 
 /-
 We introduce *barycentric* algebras, which are a commutative monoid R equipped with binary probabilistic choice functions [0,1] → R² → R where certain probabilistic inequalities hold.
-I'm too lazy to implement the interval [0,1] (as *reals aren't in mathlib4 yet!!!*) so I'm just working with Rats, courtesy of Mario.
+I'm too lazy to implement the interval [0,1], so we work with general reals.
 -/
 
 class BarycentricAlgebra (R : Type) where
@@ -27,8 +27,15 @@ class RewardOrder (R : Type) (Ord : R → R → Prop) extends IsTotal R Ord wher
   pres_choice_p : ∀ p r s t,  Ord r s ↔ Ord (bcm.choice_p p r t) (bcm.choice_p p s t)
   pres_add : ∀ r s t, Ord r s ↔ Ord (bcm.mul s t) (bcm.mul s t)
 
-class FiniteProbDistr (supp : Finset X) where
+class FiniteProbDistr (X : Type) where
   fn : X → ℝ 
-  sum_to_one : Finset.sum supp fn = 1
+  supp : Fintype (Subtype (λ x => fn x ≠ 0))
+  sum_to_one : Finset.sum supp.elems (λ x => fn x) = 1
 
+lemma noDup : Multiset.Nodup $ Multiset.ofList [1,2] := by simp
+instance : FiniteProbDistr ℕ where
+  fn := λ x => if x=1 ∨  x=2 then 1 else 0
+  supp := let M := Multiset.ofList [1,2]
+          let X := Finset.mk M
+          let y :=
 
